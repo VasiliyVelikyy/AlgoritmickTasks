138ff1c9da5e2a0b6d78df6f9a706632cf74543d
Vasiliy <algoritmick.vm@gmail.com>
2025-01-27 00:03:33 +0300

    add refactoring

--- leet-code/test/easy/RemoverDuplicatesFromSortedListTest.java
+++ leet-code/test/easy/RemoverDuplicatesFromSortedListTest.java
@@ -3,21 +3,41 @@ package easy;
 import domain.ListNode;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class RemoverDuplicatesFromSortedListTest {
     RemoverDuplicatesFromSortedList solution = new RemoverDuplicatesFromSortedList();
 
     @Test
     void deleteDuplicates1() {
-        solution.deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2))));
+        var answer = solution.deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2))));
+        String actual = getStringValFromListNode(answer);
+        assertEquals("[1,2]", actual);
+
     }
+
 
     @Test
     void deleteDuplicates2() {
-        solution.deleteDuplicates(
+        var answer = solution.deleteDuplicates(
                 new ListNode(1,
                         new ListNode(1,
                                 new ListNode(2,
                                         new ListNode(3,
                                                 new ListNode(3))))));
+        String actual = getStringValFromListNode(answer);
+        assertEquals("[1,2,3]", actual);
+    }
+
+
+    private String getStringValFromListNode(ListNode answer) {
+        StringBuilder stringBuilder = new StringBuilder("[");
+        while (answer != null) {
+            stringBuilder.append(answer.val).append(",");
+            answer = answer.next;
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        stringBuilder.append("]");
+        return stringBuilder.toString();
     }
 }
