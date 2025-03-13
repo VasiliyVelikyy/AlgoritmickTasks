d9c2b45a322cbb80e19a1988ab96fe1e013eda5b
Vasiliy <algoritmick.vm@gmail.com>
2025-03-13 17:13:05 +0300

    add other solution

--- leet-code/src/easy/binary/binary_tree/BalancedBinaryTree.java
+++ leet-code/src/easy/binary/binary_tree/BalancedBinaryTree.java
@@ -31,22 +31,50 @@ Constraints:
 The number of nodes in the tree is in the range [0, 5000].
 -104 <= Node.val <= 104*/
 public class BalancedBinaryTree {
+
+    //other solution
+    //Runtime 0 ms Beats 100.00%
+    // Memory 44.23 MB Beats 85.05%
+    public boolean isBalanced(TreeNode root) {
+        // If the tree is empty, we can say it’s balanced...
+        if (root == null)  return true;
+        // Height Function will return -1, when it’s an unbalanced tree...
+        if (height(root) == -1)  return false;
+        return true;
+    }
+    // Create a function to return the “height” of a current subtree using recursion...
+    private int height(TreeNode root) {
+        // Base case...
+        if (root == null)  return 0;
+        // Height of left subtree...
+        int leftHeight = height(root.left);
+        // Height of height subtree...
+        int rightHight = height(root.right);
+        // In case of left subtree or right subtree unbalanced, return -1...
+        if (leftHeight == -1 || rightHight == -1)  return -1;
+        // If their heights differ by more than ‘1’, return -1...
+        if (Math.abs(leftHeight - rightHight) > 1)  return -1;
+        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+        return Math.max(leftHeight, rightHight) + 1;
+    }
+
+
     //other solution
     //Runtime 0 ms Beats 100.00%
     // Memory 44.72 MB Beats 22.70%
-    public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-        else {
-            int left = getMaxSubtreeHeight(root.left);
-            int right = getMaxSubtreeHeight(root.right);
-            return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
-        }
-    }
-
-    private int getMaxSubtreeHeight(TreeNode node) {
-        if (node == null) return 0;
-        else {
-            return 1 + Math.max(getMaxSubtreeHeight(node.left), getMaxSubtreeHeight(node.right));
-        }
-    }
+//    public boolean isBalanced(TreeNode root) {
+//        if (root == null) return true;
+//        else {
+//            int left = getMaxSubtreeHeight(root.left);
+//            int right = getMaxSubtreeHeight(root.right);
+//            return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+//        }
+//    }
+//
+//    private int getMaxSubtreeHeight(TreeNode node) {
+//        if (node == null) return 0;
+//        else {
+//            return 1 + Math.max(getMaxSubtreeHeight(node.left), getMaxSubtreeHeight(node.right));
+//        }
+//    }
 }
--- leet-code/test/easy/binary/binary_tree/BalancedBinaryTreeTest.java
+++ leet-code/test/easy/binary/binary_tree/BalancedBinaryTreeTest.java
@@ -2,11 +2,7 @@ package easy.binary.binary_tree;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static easy.utils.TreeNodeUtils.*;
+import static easy.utils.TreeNodeUtils.fromIntArrayWithNullValue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -21,13 +17,25 @@ class BalancedBinaryTreeTest {
     }
 
 
-
     @Test
     void isBalanced2() {
-        var ints = fromIntArray(new Integer[]{1, 2, 2, 3, 3, null, null, 4, 4});
+        var ints = fromIntArrayWithNullValue(new Integer[]{1, 2, 2, 3, 3, null, null, 4, 4});
+        var answer = balancedBinaryTree.isBalanced(ints);
+        assertFalse(answer);
+    }
+
+    @Test
+    void isBalanced3() {
+        var ints = fromIntArrayWithNullValue(new Integer[]{3, 9, 20, null, null, 15, 7, 16, 8, null});
         var answer = balancedBinaryTree.isBalanced(ints);
         assertFalse(answer);
     }
 
 
+    @Test
+    void isBalanced4() {
+        var ints = fromIntArrayWithNullValue(new Integer[]{1,2,2,3,null,null,3,4,null,null,4});
+        var answer = balancedBinaryTree.isBalanced(ints);
+        assertFalse(answer);
+    }
 }
