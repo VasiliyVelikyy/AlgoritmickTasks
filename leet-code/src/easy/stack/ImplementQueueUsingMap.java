50c67a149926d283bef6f53a0cc9d5c3812d14e9
Vasiliy <algoritmick.vm@gmail.com>
2025-03-17 21:56:39 +0300

    implement queue with using map

---
+++ leet-code/src/easy/stack/ImplementQueueUsingMap.java
@@ -0,0 +1,39 @@
+package easy.stack;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImplementQueueUsingMap {
+    private final Map<Integer, Integer> map = new HashMap<>();
+    int index = -1;
+
+    public ImplementQueueUsingMap() {
+
+    }
+
+    public void push(int x) {
+        map.put(++index, x);
+    }
+
+    public int pop() {
+        if (index == -1) {
+            return 0;
+        }
+        var retval = map.get(index);
+        map.remove(index);
+        index--;
+        return retval;
+    }
+
+    public int peek() {
+        if (map.isEmpty()) return -1;
+        else {
+            return map.get(index);
+        }
+    }
+
+    public boolean empty() {
+        return map.isEmpty();
+    }
+}
