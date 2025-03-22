505c0e7879df3c0d0f5a579917915c745e4b72c5
Vasiliy <algoritmick.vm@gmail.com>
2025-03-22 12:33:12 +0300

    add other solution recursion

---
+++ leet-code/resourses/explanation/explonation.md
@@ -0,0 +1,4 @@
+
+### 206. Reverse Linked List
+https://github.com/VasiliyVelikyy/AlgoritmickTasks/blob/master/leet-code/src/easy/linked_list/ReverseLinkedList.java
+![img.png](media/img.png)
--- leet-code/src/easy/linked_list/ReverseLinkedList.java
+++ leet-code/src/easy/linked_list/ReverseLinkedList.java
@@ -2,8 +2,6 @@ package easy.linked_list;
 
 import common.domain.ListNode;
 
-import java.util.Stack;
-
 /*206. Reverse Linked List
 Easy
 Topics
@@ -33,28 +31,67 @@ The number of nodes in the list is the range [0, 5000].
 
 Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
 */
+
 public class ReverseLinkedList {
+
+    //My solution iteratively
+    //Runtime 0 ms Beats 100.00%
+    //Memory 42.12 MB Beats 96.49%
+//    public ListNode reverseList(ListNode head) {
+//        if(head == null ) return head;
+//        ListNode prev = null;
+//        ListNode curr = head;
+//        while (curr != null) {
+//            ListNode temp = curr.next;
+//            curr.next = prev;
+//            prev=curr;
+//            curr=temp;
+//
+//        }
+//        return prev;
+//    }
+//
+
+    //Other solution recursive
+    //Runtime 0 ms Beats 100.00%
+    //Memory 42.31 MB Beats 81.75%
+    public ListNode reverseList(ListNode head) {
+        return reverseListInt(head, null);
+    }
+
+    private ListNode reverseListInt(ListNode head, ListNode newHead) {
+        if (head == null) {
+            return newHead;
+        }
+        ListNode next = head.next;
+        head.next = newHead;
+        return reverseListInt(next, head);
+    }
+
 
     //My solution
     //Runtime 1 ms Beats 5.03%
     //Memory 42.38 MB Beats 81.75%
-    public ListNode reverseList(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        Stack<ListNode> stack = new Stack<>();
-
-        while (head != null) {
-            stack.add(head);
-            head = head.next;
-        }
-        head = stack.pop();
-        ListNode cursor = head;
-        while (!stack.isEmpty()) {
-            cursor.next = stack.pop();
-            cursor = cursor.next;
-        }
-        cursor.next = null;
-        return head;
-    }
+//    public ListNode reverseList(ListNode head) {
+//        if (head == null) {
+//            return null;
+//        }
+//        Stack<ListNode> stack = new Stack<>();
+//
+//        while (head != null) {
+//            stack.add(head);
+//            head = head.next;
+//        }
+//
+//        head = stack.pop();
+//        ListNode cursor = head;
+//
+//        while (!stack.isEmpty()) {
+//            cursor.next = stack.pop();
+//            cursor = cursor.next;
+//        }
+//
+//        cursor.next = null;
+//        return head;
+//    }
 }
