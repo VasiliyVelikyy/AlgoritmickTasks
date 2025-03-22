ff922d3359fbc9ed012317834a116289f70eb9ff
Vasiliy <algoritmick.vm@gmail.com>
2025-03-22 13:07:49 +0300

    easy task 1 for 22.03.25

--- leet-code/src/easy/array/MajorityElement.java
+++ leet-code/src/easy/array/MajorityElement.java
@@ -1,5 +1,9 @@
 package easy.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//задача странная, в условии сказано что большинство обязательно равно вхождению ⌊n / 2⌋
 /*169. Majority Element
  Given an array nums of size n, return the majority element.
 
@@ -26,33 +30,82 @@ n == nums.length
 Follow-up: Could you solve the problem in linear time and in O(1) space?
  * */
 public class MajorityElement {
+
+    //Other solution
+    //Runtime 12 ms Beats 36.07%
+    //Memory 50.16 MB Beats 71.37%
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        n = n / 2;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n) {
+                return entry.getKey();
+            }
+        }
+
+        return 0;
+    }
+
+    //My solution
+    //Runtime 16 ms Beats 18.46%
+    //Memory 49.69 MB Beats 79.49%
+//    public int majorityElement(int[] nums) {
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int num : nums) {
+//            map.put(num, map.getOrDefault(num, 0) + 1);
+//        }
+//        var list = map.entrySet()
+//                .stream()
+//                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder() ))
+//                .map(Map.Entry::getKey)
+//                .collect(Collectors.toList());
+//        return list.get(0);
+//    }
+
+    //My solution
+    //Runtime 15 msBeats22.74%
+    //Memory 49.80 MB Beats 74.81%
 //    public int majorityElement(int[] nums) {
 //      HashMap<Integer,Integer>map=new HashMap<>();
 //        for (int current:nums){
 //                map.put(current,map.getOrDefault(current,0)+1);
 //        }
-//         int max= map.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).get().getKey();
+//         int max= map.entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
 //        return max;
 //    }
 
-    public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0;
 
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
+//    public int majorityElement(int[] nums) {
+//        int count = 0;
+//        int candidate = 0;
+//
+//        for (int num : nums) {
+//            if (count == 0) {
+//                candidate = num;
+//            }
+//            if (num == candidate) {
+//                count++;
+//            } else {
+//                count--;
+//            }
+//        }
+//        return candidate;
+//    }
 
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
-            }
-        }
 
-        return candidate;
-    }
+    //Runtime 4 ms Beats 51.62%
+    //Memory 52.96 MB Beats 65.47%
+//    public int majorityElement(int[] nums) {
+//        Arrays.sort(nums);
+//        int n = nums.length;
+//        return nums[n/2];
+//    }
 
 
 }
--- leet-code/test/easy/array/MajorityElementTest.java
+++ leet-code/test/easy/array/MajorityElementTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+//задача странная, в условии сказано что большинство обязательно равно вхождению ⌊n / 2⌋
+//то есть нужно следить за количеством вхождений. Если 6 элементов массива то большинство должно быть больше 3
 class MajorityElementTest {
 
     MajorityElement solution = new MajorityElement();
@@ -20,6 +22,22 @@ class MajorityElementTest {
     void majorityElement2() {
         int expected = 2;
         int actual = solution.majorityElement(new int[]{2, 2, 1, 1, 1, 2, 2});
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void majorityElement3() {
+        int expected = 3;
+        int actual = solution.majorityElement(new int[]{3, 3, 2, 3});
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    void majorityElement4() {
+        int expected = 5;
+        int actual = solution.majorityElement(new int[]{5, 5, 5, 2, 3, 5});
         assertEquals(expected, actual);
     }
 
