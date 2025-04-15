7bffe4daa6416aaec4aef1e5616809562534e049
Vasiliy <algoritmick.vm@gmail.com>
2025-04-15 14:13:40 +0300

    add second easy task 15.04.25

--- leet-code/src/main/java/easy/sequence/ContainsDuplicate.java
+++ leet-code/src/main/java/easy/sequence/ContainsDuplicate.java
@@ -1,6 +1,7 @@
 package easy.sequence;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /*
 217. Contains Duplicate
@@ -26,6 +27,29 @@ Constraints:
 1 <= nums.length <= 105
 -109 <= nums[i] <= 109*/
 public class ContainsDuplicate {
+
+    //Runtime 14 ms Beats 40.78%
+    // Memory 58.68 MB Beats 25.68%
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) return true;
+            else set.add(num);
+
+        }
+        return false;
+    }
+
+    //Runtime 20 ms Beats 23.20%
+    //Memory 59.74 MB Beats 14.41%
+//    public boolean containsDuplicate(int[] nums) {
+//        Set<Integer> set = new HashSet<>();
+//        for (int num : nums) {
+//            set.add(num);
+//        }
+//        return nums.length != set.size();
+//    }
+
 
     //Solution 1 BruteForce .
     // Difficult O(n^2), this solution is not effective for large array
@@ -68,15 +92,15 @@ public class ContainsDuplicate {
     // Result :
     // runtime 11 ms
     // memory 54,96
-    public boolean containsDuplicate(int[] nums) {
-        Set<Integer> integerSet = new HashSet<>();
-        for (int num : nums) {
-            if (integerSet.contains(num)) return true;
-            else
-                integerSet.add(num);
-        }
-        return false;
-    }
+//    public boolean containsDuplicate(int[] nums) {
+//        Set<Integer> integerSet = new HashSet<>();
+//        for (int num : nums) {
+//            if (integerSet.contains(num)) return true;
+//            else
+//                integerSet.add(num);
+//        }
+//        return false;
+//    }
 
     //Solution 4
     // Result :
