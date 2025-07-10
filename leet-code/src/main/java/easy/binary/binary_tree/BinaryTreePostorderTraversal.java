480268cee30f4e3d88f68caac558a046051ad864
Vasiliy <algoritmick.vm@gmail.com>
2025-07-11 01:44:36 +0300

    add two easy tasks 10.05.25

--- leet-code/src/main/java/easy/binary/binary_tree/BinaryTreePostorderTraversal.java
+++ leet-code/src/main/java/easy/binary/binary_tree/BinaryTreePostorderTraversal.java
@@ -7,12 +7,12 @@ import java.util.List;
 import java.util.Stack;
 
 /*
-* 144. Binary Tree Preorder Traversal
+* 145. Binary Tree Postorder Traversal
 Easy
 Topics
 premium lock icon
 Companies
-Given the root of a binary tree, return the preorder traversal of its nodes' values.
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
 
 
 
@@ -20,14 +20,23 @@ Example 1:
 
 Input: root = [1,null,2,3]
 
-Output: [1,2,3]
-* Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
-
-Output: [1,2,4,5,6,7,3,8,9]
+Output: [3,2,1]
 
 Explanation:
-*
-* Example 3:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,6,7,5,2,9,8,3,1]
+
+Explanation:
+
+
+
+Example 3:
 
 Input: root = []
 
@@ -38,54 +47,52 @@ Example 4:
 Input: root = [1]
 
 Output: [1]
-* Constraints:
 
-The number of nodes in the tree is in the range [0, 100].
+
+
+Constraints:
+
+The number of the nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 
 
 Follow up: Recursive solution is trivial, could you do it iteratively?*/
-public class BinaryTreePreorderTraversal {
-
-    //My solution
-    //Runtime 0 ms Beats 100.00%
-    //Memory 41.89 MB Beats 27.44%
-    public List<Integer> preorderTraversal(TreeNode root) {
+public class BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) return result;
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
-
         while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            result.add(node.val);
+            TreeNode node = stack.peek();
 
             if (node.right != null) {
                 stack.push(node.right);
             }
             if (node.left != null) {
                 stack.push(node.left);
+            } else if (node.left == null && node.right == null) {
+                stack.pop();
+                result.add(node.val);
             }
         }
         return result;
     }
 
-
-    //My solution
-    //Runtime 0 ms Beats 100.00%
-    //Memory 41.75 MB Beats 44.49%
-//    public List<Integer> preorderTraversal(TreeNode root) {
-//        if (root == null) return new ArrayList<>();
+    //My Solution
+    // Runtime 0 ms Beats 100.00%
+    //Memory 41.63 MB Beats 65.67%
+//    public List<Integer> postorderTraversal(TreeNode root) {
 //        List<Integer> result = new ArrayList<>();
-//        addValue(root, result);
+//        if (root == null) return result;
+//        addValue(result, root);
 //        return result;
 //    }
 //
-//    private void addValue(TreeNode root, List<Integer> result) {
+//    private void addValue(List<Integer> result, TreeNode root) {
 //        if (root == null) return;
+//        addValue(result, root.left);
+//        addValue(result, root.right);
 //        result.add(root.val);
-//        addValue(root.left, result);
-//        addValue(root.right, result);
 //    }
 }
-
---
+++ leet-code/src/test/java/easy/binary/binary_tree/BinaryTreePostorderTraversalTest.java
@@ -0,0 +1,38 @@
+package easy.binary.binary_tree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static utils.TreeNodeCollectors.fromIntArrayWithNullValue;
+
+class BinaryTreePostorderTraversalTest {
+    BinaryTreePostorderTraversal solution = new BinaryTreePostorderTraversal();
+
+    @Test
+    void postorderTraversal() {
+        var nodes = fromIntArrayWithNullValue(new Integer[]{1, null, 2, 3});
+        var result = solution.postorderTraversal(nodes);
+        assertArrayEquals(new Integer[]{3, 2, 1}, result.toArray());
+    }
+
+    @Test
+    void postorderTraversa2() {
+        var nodes = fromIntArrayWithNullValue(new Integer[]{1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9});
+        var result = solution.postorderTraversal(nodes);
+        assertArrayEquals(new Integer[]{4, 6, 7, 5, 2, 9, 8, 3, 1}, result.toArray());
+    }
+
+    @Test
+    void postorderTraversa3() {
+        var nodes = fromIntArrayWithNullValue(new Integer[]{});
+        var result = solution.postorderTraversal(nodes);
+        assertArrayEquals(new Integer[]{}, result.toArray());
+    }
+
+    @Test
+    void postorderTraversa4() {
+        var nodes = fromIntArrayWithNullValue(new Integer[]{1});
+        var result = solution.postorderTraversal(nodes);
+        assertArrayEquals(new Integer[]{1}, result.toArray());
+    }
+}
